82247445a502a38a4f029ec9c6dd61b5b68a5f8d
paratechnical <paraschiv.f.andrei@gmail.com>
2018-06-29 13:08:34 +0300

    added my code

---
+++ src/main/java/org/cdp/okhttpoauth2/OAuthResponseCallback.java
@@ -0,0 +1,5 @@
+package org.cdp.okhttpoauth2;
+
+public interface OAuthResponseCallback {
+    void onResponse(OAuthResponse response);
+}
---
+++ src/main/java/org/cdp/okhttpoauth2/Token.java
@@ -0,0 +1,9 @@
+package org.cdp.okhttpoauth2;
+
+class Token {
+    protected Long expires_in;
+    protected String token_type;
+    protected String refresh_token;
+    protected String access_token;
+    protected String scope;
+}
